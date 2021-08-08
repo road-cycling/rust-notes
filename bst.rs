b31d7f5c45f68b1c3b79dbfb260f0cf49aba65e7
Nathan <nathan_kamm@yahoo.com>
2021-08-07 21:48:16 -0700

    Update bst.rs

--- bst.rs
+++ bst.rs
@@ -1,7 +1,9 @@
 use std::io;
 use std::cmp::max;
 use std::collections::VecDeque;
-use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::borrow::{Borrow, BorrowMut};
+use std::ops::Index;
 
 #[derive(Debug)]
 struct BST {
@@ -64,31 +66,58 @@ impl BST {
             None => 0
         }
     }
-    
+
     fn level_order_traversal_map(node: Option<Box<BST>>) {
+
+        let mut levels: HashMap<i32, Vec<u32>> = HashMap::new();
+
+        fn level_order_traversal(level_map: &mut HashMap<i32, Vec<u32>>, node: Option<Box<BST>>, level: i32) {
+
+            match node {
+                Some(tree) => {
+
+                    let entry = level_map
+                        .entry(level)
+                        .or_insert(Vec::new());
+                    entry.push(tree.value);
+
+                    level_order_traversal(level_map, tree.left_node, level + 1);
+                    level_order_traversal(level_map, tree.right_node, level + 1);
+                },
+                None => {}
+            }
+        }
+
+        level_order_traversal(&mut levels, node, 1);
+
+        for level in levels.borrow().keys() {
+            println!("Level: {} - {:?}", level, levels.index(level))
+        }
+
+    }
+
+    fn level_order_traversal_queue(node: Option<Box<BST>>) {
 
         let mut queue: VecDeque<Box<BST>> = VecDeque::new();
         queue.push_back(node.unwrap_or(BST::bst_on_heap(0)));
 
 
-        while queue.borrow().len() != 0 {
-
+        while queue.len() != 0 {
             let queue_size = queue.len();
 
             for _ in 0..queue_size {
 
-                let head = queue.front_mut().unwrap();
-
+                let head = queue.pop_front().unwrap();
                 print!("{} ", head.value);
 
                 match head.left_node {
                     Some(left_node) => queue.push_back(left_node),
-                    _ => continue,
+                    None => {},
                 }
 
                 match head.right_node {
                     Some(right_node) => queue.push_back(right_node),
-                    _ => continue,
+                    None => {},
                 }
 
             }
@@ -96,8 +125,24 @@ impl BST {
         }
     }
 
+    fn nth_node_in_tree(node: Option<Box<BST>>, nth: &mut i32) -> Option<u32> {
+        match node {
+            Some(n) => {
+                match BST::nth_node_in_tree(n.left_node, nth) {
+                    Some(v) => Some(v),
+                    None => {
+                        if *nth == 0 {
+                            return Some(n.value)
+                        }
+                        *nth -= 1;
 
-
+                        BST::nth_node_in_tree(n.right_node, nth)
+                    },
+                }
+            },
+            None => None
+        }
+    }
 }
 
 pub(crate) fn create_bst() {
@@ -126,7 +171,8 @@ pub(crate) fn create_bst() {
 
     }
 
+    // Issue with borrow here -- need to reread docs.
+    // BST::level_order_traversal_queue(root);
     BST::level_order_traversal_map(root);
-
 
 }
